685f34b2fc4357188d0bc058ef368b14885284de
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2026-07-20 12:27:37 +0530

    main

--- app/Http/Controllers/WardController.php
+++ app/Http/Controllers/WardController.php
@@ -561,56 +561,57 @@ class WardController extends Controller
         }
     }
 
-   public function missingBuiilding($ward_id)
-{
-    try {
+    public function missingBuiilding($ward_id)
+    {
+        try {
 
-        $polygonDataTable = "polygon_data_" . $ward_id;
-        $polygonTable     = "polygons_" . $ward_id;
+            $polygonDataTable = "polygon_data_" . $ward_id;
+            $polygonTable     = "polygons_" . $ward_id;
 
-        $missingBuildings = DB::table($polygonTable)
-            ->whereNotIn('gisid', function ($query) use ($polygonDataTable) {
-                $query->select('gisid')
-                      ->from($polygonDataTable);
-            })
-            ->get();
+            $missingBuildings = DB::table($polygonTable)
+                ->whereNotIn('gisid', function ($query) use ($polygonDataTable) {
+                    $query->select('gisid')
+                        ->from($polygonDataTable);
+                })
+                ->get();
 
-        $features = [];
+            $features = [];
 
-        foreach ($missingBuildings as $building) {
+            foreach ($missingBuildings as $building) {
 
-            $geometry = json_decode($building->feature, true);
+                $features[] = [
+                    "type" => "Feature",
+                    "geometry" => [
+                        "type" => $building->type,
+                        "coordinates" => json_decode($building->coordinates, true)
+                    ],
+                    "properties" => [
+                        "gisid" => $building->gisid,
+                        "sqfeet" => $building->sqfeet,
+                    ]
+                ];
+            }
 
-            $features[] = [
-                "type" => "Feature",
-                "geometry" => $geometry,
-                "properties" => [
-                    "gisid" => $building->gisid,
-                ]
+            $geojson = [
+                "type" => "FeatureCollection",
+                "features" => $features
             ];
+
+            $filename = "missing_buildings_{$ward_id}.geojson";
+
+            return response()->streamDownload(function () use ($geojson) {
+                echo json_encode($geojson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+            }, $filename, [
+                'Content-Type' => 'application/geo+json',
+            ]);
+        } catch (\Throwable $e) {
+
+            return response()->json([
+                "success" => false,
+                "message" => $e->getMessage(),
+                "line" => $e->getLine(),
+                "file" => $e->getFile(),
+            ], 500);
         }
-
-        $geojson = [
-            "type" => "FeatureCollection",
-            "features" => $features
-        ];
-
-        $filename = "missing_buildings_{$ward_id}.geojson";
-
-        return response()->streamDownload(function () use ($geojson) {
-            echo json_encode($geojson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-        }, $filename, [
-            'Content-Type' => 'application/geo+json',
-        ]);
-
-    } catch (\Throwable $e) {
-
-        return response()->json([
-            "success" => false,
-            "message" => $e->getMessage(),
-            "line"    => $e->getLine(),
-            "file"    => $e->getFile(),
-        ], 500);
     }
-}
 }
