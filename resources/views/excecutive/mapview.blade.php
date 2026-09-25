887766d86b7e0a547c120a52b474f50e869b6460
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2026-09-25 09:19:56 +0530

    main

--- resources/views/excecutive/mapview.blade.php
+++ resources/views/excecutive/mapview.blade.php
@@ -603,7 +603,7 @@
         .bld-image-strip {
             display: flex;
             gap: 0;
-            height: 220px;
+            height: 500px;
             background: #0f172a;
         }
 
@@ -622,7 +622,7 @@
         }
 
         .bld-img-wrap img {
-            width: 100%;
+            /* width: 100%; */
             height: 100%;
             object-fit: cover;
             transition: transform .4s ease;
